4e81888322b897b5b914ae21555baff324f2e5ca
xolani_masuku <mcxolani@gmail.com>
2017-04-03 20:41:26 +0200

    add add location endpoint

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 var express = require('express')
 var app = express()
 var cors = require('cors')
+var bodyParser = require('body-parser')
 
+var locations = [];
 app.use(cors())
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/locations', function (req, res) {
+  res.json(locations);
+})
+app.post('/location/add/:id', function (req, res) {
+    locations.push(req.body);
+    console.log(req.body);
+  res.json({success:true});
 })
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080')
+  console.log('listening on port 8080')
 });
